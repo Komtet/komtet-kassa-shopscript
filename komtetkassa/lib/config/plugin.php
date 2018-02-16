1da13c2e56898dd14bf49ef6b064e38d5e016ea3
elatonsev <elatonsev.mihail@gmail.com>
2018-02-16 14:48:48 +0300

    v 1.2.4

--- komtetkassa/lib/config/plugin.php
+++ komtetkassa/lib/config/plugin.php
@@ -3,7 +3,7 @@
 return array(
     'name' => 'КОМТЕТ Касса',
     'description' => 'Фискализация платежей с помощью сервиса КОМТЕТ Касса',
-    'version' => '1.2.3',
+    'version' => '1.2.4',
     'vendor' => 1087963,
     'frontend' => true,
     'handlers' => array(
