75c89164d5b202844125a7252a8432723f414e41
Elatonsev Mihail <elatonsev.mihail@gmail.com>
2019-01-30 17:40:36 +0300

    Поднял версию

--- komtetkassa/lib/config/plugin.php
+++ komtetkassa/lib/config/plugin.php
@@ -3,7 +3,7 @@
 return array(
     'name' => 'КОМТЕТ Касса',
     'description' => 'Фискализация платежей с помощью сервиса КОМТЕТ Касса',
-    'version' => '1.2.6',
+    'version' => '1.2.7',
     'vendor' => 1087963,
     'frontend' => true,
     'handlers' => array(
