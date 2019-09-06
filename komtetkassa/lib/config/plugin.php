7b8f396667a90ca0af09796f5f39a4c387635315
Михаил Елатонцев <elatonsev.mihail@gmail.com>
2019-09-06 17:51:27 +0300

    Точности округления перед использованием функции `json_encode` выставлена для всех версий php

--- komtetkassa/lib/config/plugin.php
+++ komtetkassa/lib/config/plugin.php
@@ -3,7 +3,7 @@
 return array(
     'name' => 'КОМТЕТ Касса',
     'description' => 'Фискализация платежей с помощью сервиса КОМТЕТ Касса',
-    'version' => '1.2.12',
+    'version' => '1.2.13',
     'vendor' => 1087963,
     'frontend' => true,
     'handlers' => array(
