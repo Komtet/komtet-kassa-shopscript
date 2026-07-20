f18e3ee3391d3c27882740a02094433a3dda88b5
Коновалов Александр <aleksandrwork.motmom@gmail.com>
2026-07-20 12:42:04 +0300

    release 3.0.0

--- komtetkassa/lib/config/plugin.php
+++ komtetkassa/lib/config/plugin.php
@@ -3,7 +3,7 @@
 return array(
     'name' => 'КОМТЕТ Касса',
     'description' => 'Фискализация платежей с помощью сервиса КОМТЕТ Касса',
-    'version' => '2.4.0',
+    'version' => '3.0.0',
     'vendor' => 1087963,
     'frontend' => true,
     'handlers' => array(
